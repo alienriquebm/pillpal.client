65217ac27eae1d727186b7aae678346e8080a237
Ali Briceño <alienriquebm@icloud.com>
2025-06-13 23:45:23 -0400

    feat: add testing dependencies and configure Vitest setup

--- test.setup.ts
+++ test.setup.ts
@@ -1,6 +1,20 @@
-import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // API legacy
+    removeListener: () => {},
+    addEventListener: () => {}, // API moderna
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
+
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
   cleanup();
